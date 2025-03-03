7542cb5bae75c78f180898361779a448c2793305
luis12cj <159955218+luis12cj@users.noreply.github.com>
2025-03-02 21:48:51 -0600

    Add files via upload

---
+++ Historia del Internet/Historia-del-Internet/src/main/java/com/upiiz/history/HistoriaDelInternetApplication.java
@@ -0,0 +1,13 @@
+package com.upiiz.history;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HistoriaDelInternetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HistoriaDelInternetApplication.class, args);
+	}
+
+}
